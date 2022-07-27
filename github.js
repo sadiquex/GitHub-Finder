85fea7097792eb06de9e5a21481de8a2d1080ce0
sadiquex <abubakasaddik1@gmail.com>
2022-07-27 08:08:23 +0000

    fetched data from github api

---
+++ github.js
@@ -0,0 +1,18 @@
+class Github {
+  constructor() {
+    this.client_id = "fe26ff6802395ea62305";
+    this.client_secret = "a46cf5fbd24249edb39582d247b02952a35afc80";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id${this.client_id}&client_secret${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile,
+    };
+  }
+}
